ce5bcd015829259e98aa75264ee3dab6e5264c23
Mukul Mantosh <Mukul.Mantosh@in.bosch.com>
2022-06-15 06:25:02 +0530

    Feat: -> Initial Commit

---
+++ data/management/commands/create_dummy_user.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create Sample User'
+
+    def handle(self, *args, **kwargs):
+        User.objects.create_user(username='mukulmantosh', password='admin123',
+                                 is_staff=True, is_active=True, is_superuser=True)
